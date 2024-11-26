61267b50fc2418ce74e4d6e51f983ae33f81ff9a
Cristian Chacon <cristianchacohn@gmail.com>
2024-11-26 20:42:43 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generateCard() {
+  const suits = ["heart", "diamond", "spade", "club"];
+  const suitSymbols = { heart: "♥", diamond: "♦", spade: "♠", club: "♣" };
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+  const randomNumberIndex = Math.floor(Math.random() * numbers.length);
+
+  const suit = suits[randomSuitIndex];
+  const number = numbers[randomNumberIndex];
+
+  const card = document.getElementById("card");
+
+  card.className = `card-container ${suit}`;
+  card.innerHTML = `
+    <div class="top-suit">${suitSymbols[suit]}</div>
+    <div class="number">${number}</div>
+    <div class="bottom-suit">${suitSymbols[suit]}</div>
+  `;
+}
+
+window.onload = generateCard;
